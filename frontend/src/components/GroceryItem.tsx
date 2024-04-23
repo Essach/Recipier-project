ab4c7d3b363d7e9ce6a9758743cf838a0552a16c
Adrian <adrian.wozny@interia.pl>
2024-04-23 20:23:33 +0200

    feat. detect grocery changes (to save)

--- frontend/src/components/GroceryItem.tsx
+++ frontend/src/components/GroceryItem.tsx
@@ -1,11 +1,90 @@
-import { Box, Heading } from "@chakra-ui/react";
-import { GroceryProps } from "../pages/Groceries";
+import { AspectRatio, Box, Card, CardBody, Editable, EditableInput, EditablePreview, Heading, IconButton, Image } from "@chakra-ui/react";
+import { AddIcon, MinusIcon } from "@chakra-ui/icons";
+import { useState } from "react";
+import { GroceryAction } from "../pages/Groceries";
 
-const GroceryItem = (props: GroceryProps) => {
+const GroceryItem = ({id, image,name,quantity,quantityType, dispatch}: {id: string,image: string,name: string,quantity: string, quantityType: string, dispatch: (arg0: GroceryAction) => void}) => {
+    const [currentQuantity, setCurrentQuantity] = useState<string>(quantity);
+    
+    const handleChangeQuantity = (v: string) => {
+        if (parseInt(v) >= 0 || v === "") {
+            setCurrentQuantity(v)
+        }
+        if (parseInt(v) !== parseInt(quantity)) {
+            dispatch({ type: 'ADD', payload: { id: id, newQuantity: parseInt(v) } })
+        } else if (parseInt(v) === parseInt(quantity)) {
+            dispatch({ type: 'DELETE', payload: { id: id } })
+        }
+    }
+    const handleOnBlurQuantity = () => {
+        if (currentQuantity === "") {
+            setCurrentQuantity(quantity)
+        }
+    }
+    const handleAddQuantity = () => {
+        setCurrentQuantity((parseInt(currentQuantity) + 1).toString())
+
+        if (parseInt(currentQuantity)+1 !== parseInt(quantity)) {
+            dispatch({ type: 'ADD', payload: { id: id, newQuantity: parseInt(currentQuantity)+1 } })
+        } else if (parseInt(currentQuantity)+1 === parseInt(quantity)) {
+            dispatch({ type: 'DELETE', payload: { id: id } })
+        }
+    }
+    const handleSubtractQuantity = () => {
+        if (parseInt(currentQuantity) > 0) {
+            setCurrentQuantity((parseInt(currentQuantity) - 1).toString())
+        }
+        if (parseInt(currentQuantity) - 1 !== parseInt(quantity)) {
+            dispatch({ type: 'ADD', payload: { id: id, newQuantity: parseInt(currentQuantity) - 1 } })
+        } else if (parseInt(currentQuantity)-1 === parseInt(quantity)) {
+            dispatch({ type: 'DELETE', payload: { id: id } })
+        }
+    }
+
     return (
-        <Box>
-            <Heading>{props.name}</Heading>
-        </Box>
+        <Card maxW='700px'>
+            <CardBody display='flex' alignItems='center' justifyContent='space-between' p='12px'>
+                <Box display='flex' alignItems='center'>
+                    <Image
+                        src={image}
+                        objectFit='cover'
+                        boxSize='60px'
+                        aspectRatio={1}
+                    />
+                    <Heading size='lg'>{name.charAt(0).toUpperCase() + name.slice(1)}</Heading>
+                </Box>
+                <Box display='flex' alignItems='center'>
+                    <IconButton
+                        aria-label='Add 1 of this grocery'
+                        icon={<AddIcon />}
+                        colorScheme='green'
+                        onClick={handleAddQuantity}
+                    />
+                    <AspectRatio w='40px' ratio={1}>
+                        <Editable
+                            value={currentQuantity}
+                            textAlign='center'
+                            fontSize='xl'
+                        >
+                            <EditablePreview />
+                            <EditableInput
+                                value={currentQuantity}
+                                onChange={(e) => { handleChangeQuantity(e.target.value) }}
+                                type="number"
+                                onBlur={handleOnBlurQuantity}
+                                />
+                        </Editable>
+                    </AspectRatio>
+                    <IconButton
+                        aria-label='Subtract 1 of this grocery'
+                        icon={<MinusIcon />}
+                        colorScheme='red'
+                        onClick={handleSubtractQuantity}
+                    />
+                    <Heading size='md' ml='10px'>{quantityType}s</Heading>
+                </Box>
+            </CardBody>
+        </Card>
     );
 }
 
--- frontend/src/pages/Groceries.tsx
+++ frontend/src/pages/Groceries.tsx
@@ -1,5 +1,5 @@
-import { Button, Flex, Heading, Input, Stack } from "@chakra-ui/react";
-import { useContext } from "react";
+import { AlertDialog, AlertDialogBody, AlertDialogContent, AlertDialogHeader, Button, Flex, Heading, Input, Stack, useDisclosure } from "@chakra-ui/react";
+import { Reducer, useContext, useEffect, useReducer, useRef } from "react";
 import { StoreContext } from "../StoreProvider";
 import GroceryItem from "../components/GroceryItem";
 
@@ -11,33 +11,86 @@ export interface GroceryProps {
     quantityType: string
 }
 
+interface GroceryChange {
+    id: string,
+    newQuantity?: number;
+}
+
+export interface GroceryAction {
+    type: 'ADD' | 'DELETE';
+    payload: GroceryChange;
+}
+
 const Groceries = () => {
 
     const { groceries } = useContext(StoreContext);
 
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    
+    const cancelRef = useRef(null)
+
+    const changesLogReducer = (changesLog: Array<GroceryChange>, action: GroceryAction) => {
+        const { type, payload } = action;
+        switch (type) {
+            case 'ADD': {
+                if (changesLog.findIndex(grocery => grocery.id === payload.id) === -1) {
+                    return [...changesLog, payload]
+                } else {
+                    return changesLog
+                }
+            }
+            case 'DELETE':
+                return changesLog.filter(grocery => grocery.id !== payload.id)
+            default:
+                return changesLog;
+        }
+    }
+
+    const [changesLog, dispatch] = useReducer(changesLogReducer, [])
+
+    useEffect(() => {
+        console.log(changesLog)
+    },[changesLog])
+
     return (
-        <Stack width='100%' gap='30px'>
-            <Heading size='4xl'>Groceries</Heading>
-            <Flex gap='30px'>
-                <Input
-                    placeholder='Search for an item...'
-                    size='md'
-                    w='350px'
-                    variant='outline'
-                />
-                <Button variant='solid' colorScheme='teal'>
-                    Add new grocery
-                </Button>
-            </Flex>
-            <Stack>
-                {groceries.map((grocery: GroceryProps) => (
-                    <GroceryItem {...grocery}
-                        // id={grocery.id}
-                        // name={grocery.name}
-                        // image={grocery.image}
-                    />))}
+        <>
+            <Stack width='100%' gap='30px'>
+                <Heading size='4xl'>Groceries</Heading>
+                <Flex gap='30px'>
+                    <Input
+                        placeholder='Search for an item...'
+                        size='md'
+                        w='350px'
+                        variant='outline'
+                    />
+                    <Button variant='solid' colorScheme='teal'>
+                        Add new grocery
+                    </Button>
+                </Flex>
+                <Stack>
+                    {groceries.map((grocery: GroceryProps) => (
+                        <GroceryItem key={grocery.id}
+                            {...grocery}
+                            dispatch={dispatch}
+                        />))}
+                </Stack>
             </Stack>
-        </Stack>
+            <AlertDialog
+                isOpen={isOpen}
+                leastDestructiveRef={cancelRef}
+                onClose={onClose}
+            >
+                <AlertDialogContent>
+                    <AlertDialogHeader>
+                        Save changes?
+                    </AlertDialogHeader>
+                    <AlertDialogBody>
+                        <Button colorScheme="green" onClick={onClose}></Button>
+                        <Button colorScheme="red" ref={cancelRef} onClick={onClose}></Button>
+                    </AlertDialogBody>
+                </AlertDialogContent>
+            </AlertDialog>
+        </>
     );
 }
 
