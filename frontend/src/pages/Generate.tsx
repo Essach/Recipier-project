e7680ebdec1c6550dfe59a1534af640140ad3412
Adrian <adrian.wozny@interia.pl>
2024-05-20 16:52:40 +0200

    feat. decrease groceries after choosing recipe

--- frontend/src/pages/Generate.tsx
+++ frontend/src/pages/Generate.tsx
@@ -1,12 +1,13 @@
-import { Box, Button, ButtonGroup, Card, CardBody, CardFooter, CardHeader, Center, Divider, Heading, Image, Spinner, UnorderedList } from "@chakra-ui/react";
+import { Box, Button, ButtonGroup, Card, CardBody, CardFooter, CardHeader, Center, Divider, Heading, Image, Modal, ModalContent, ModalOverlay, Spinner, UnorderedList, useDisclosure } from "@chakra-ui/react";
 import { useContext, useState } from "react";
 import { StoreContext } from "../StoreProvider";
 import { RecipeIngredientProps, RecipeProps } from "./Recipes";
 import '../styles/generatedRecipe.css';
 import GeneratedRecipeIngredient from "../components/GeneratedRecipeIngredient";
+import request from "../helpers/request";
 
 const Generate = () => {
-    const {recipes, groceries} = useContext(StoreContext)
+    const {recipes, groceries, setGroceries} = useContext(StoreContext)
 
     const [currentScene, setCurrentScene] = useState<'button' | 'spinner' | 'recipe' | 'error'>('button')
     
@@ -15,6 +16,17 @@ const Generate = () => {
     const [generatedRecipe, setGeneratedRecipe] = useState<RecipeProps>()
 
     const [cardKey, setCardKey] = useState(0)
+
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
+    const fetchGroceriesData = async () => {
+        const { data, status } = await request.get('/groceries');
+
+        if (status === 200) {
+            console.log(data.groceries)
+            setGroceries(data.groceries)
+        }
+    }
 
     const generateRecipe = () => {
         const possibleRecipes: Array<RecipeProps> = [];
@@ -59,55 +71,94 @@ const Generate = () => {
             return
         }
 
+        
+
         setCardKey(Math.random())
         setGeneratedRecipe(generatedRecipe)
         setCurrentScene('recipe')
         
     }
 
+    const handleOnClickChoose = async () => {
+        onOpen()
+        if (generatedRecipe !== undefined) {
+            let isAllGood = true;
+
+            for (const ingredient of generatedRecipe.ingredients) {
+                const grocery = groceries.find(g => g.name === ingredient.name);
+                if (grocery) {
+                    const newQuantity = grocery.quantity - ingredient.quantity;
+
+                    const { data, status } = await request.patch('/groceries/quantity', { groceryId: grocery.id, updatedQuantity: newQuantity })
+                
+                    console.log(ingredient.name, status)
+
+                    if (status !== 200) {
+                        isAllGood = false;
+                        throw new Error(data.message);
+                    }
+                }
+            }
+
+            if (isAllGood) {
+                console.log('here')
+                onClose();
+                fetchGroceriesData()
+            }
+        }
+    }
+
 
     return (
-        <Center h='100vh'>
-            {currentScene === 'button' && <Button height='200px' width='500px' colorScheme='teal' _active={{transform: 'scale(0.90,0.92)', transition: 'transform 0.2s ease'}} onClick={() =>setTimeout(handleOnClickGenerate, 50)}>
-                <Heading size='4xl'>Generate</Heading>
-            </Button>
-            }
-            {currentScene === 'spinner' &&
-                <Spinner thickness='10px' speed='0.65s' color='blue.500' h='200px' w='200px' />
-            }
-            {currentScene === 'error' && <Heading textAlign='center'>
-                {errorText}
-            </Heading>}   
-            {(currentScene === 'recipe' && generatedRecipe !== undefined) &&
-            <Card className="generated-recipe" key={cardKey}>
-                <CardHeader pb='0'>
-                    <Heading>
-                        {generatedRecipe.name}
-                    </Heading>
-                </CardHeader>
-                <CardBody display='flex' justifyContent='center' flexDirection='column'>
-                    <Image src={generatedRecipe.image.url}/>
-                    <Box>
-                        <UnorderedList display='grid' gridAutoFlow='column'
-                        gridTemplateColumns='repeat(2, 1fr)'
-                        pt='1.25rem'>
-                                {generatedRecipe.ingredients.map((ingredient: RecipeIngredientProps) => <GeneratedRecipeIngredient key={Math.random()} {...ingredient} />)}
-                        </UnorderedList>
-                    </Box>
-                    </CardBody>
-                    <Divider/>
-                    <CardFooter>
-                        <ButtonGroup spacing='2' display='flex' justifyContent='right' w='100%'>
-                            <Button variant='ghost' colorScheme="teal" onClick={handleOnClickGenerate}>
-                                Generate again
-                            </Button>
-                            <Button variant='solid' colorScheme="teal">
-                                Choose this recipe
-                            </Button>
-                        </ButtonGroup>
-                    </CardFooter>
-            </Card>}
-        </Center>
+        <>
+            <Modal isOpen={isOpen} onClose={onClose} closeOnEsc={false} closeOnOverlayClick={false} isCentered={true}>
+                <ModalOverlay/>
+                <ModalContent bg='transparent' boxShadow='none' display='flex' justifyContent='center' alignItems='center'>
+                    <Spinner color='blue.500' thickness='10px' speed='0.65s' h='14rem' w='14rem' />
+                </ModalContent>
+            </Modal>
+            <Center h='100vh'>
+                {currentScene === 'button' && <Button height='200px' width='500px' colorScheme='teal' _active={{transform: 'scale(0.90,0.92)', transition: 'transform 0.2s ease'}} onClick={() =>setTimeout(handleOnClickGenerate, 50)}>
+                    <Heading size='4xl'>Generate</Heading>
+                </Button>
+                }
+                {currentScene === 'spinner' &&
+                    <Spinner thickness='10px' speed='0.65s' color='blue.500' h='200px' w='200px' />
+                }
+                {currentScene === 'error' && <Heading textAlign='center'>
+                    {errorText}
+                </Heading>}   
+                {(currentScene === 'recipe' && generatedRecipe !== undefined) &&
+                <Card className="generated-recipe" key={cardKey}>
+                    <CardHeader pb='0'>
+                        <Heading>
+                            {generatedRecipe.name}
+                        </Heading>
+                    </CardHeader>
+                    <CardBody display='flex' justifyContent='center' flexDirection='column'>
+                        <Image src={generatedRecipe.image.url}/>
+                        <Box>
+                            <UnorderedList display='grid' gridAutoFlow='column'
+                            gridTemplateColumns='repeat(2, 1fr)'
+                            pt='1.25rem'>
+                                    {generatedRecipe.ingredients.map((ingredient: RecipeIngredientProps) => <GeneratedRecipeIngredient key={Math.random()} {...ingredient} />)}
+                            </UnorderedList>
+                        </Box>
+                        </CardBody>
+                        <Divider/>
+                        <CardFooter>
+                            <ButtonGroup spacing='2' display='flex' justifyContent='right' w='100%'>
+                                <Button variant='ghost' colorScheme="teal" onClick={handleOnClickGenerate}>
+                                    Generate again
+                                </Button>
+                                <Button variant='solid' colorScheme="teal" onClick={handleOnClickChoose}>
+                                    Choose this recipe
+                                </Button>
+                            </ButtonGroup>
+                        </CardFooter>
+                </Card>}
+            </Center>
+        </>
     )
 }
 
