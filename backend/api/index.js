48a4134216b64c78415864d1c1d0aa1ca6890d09
Adrian <adrian.wozny@interia.pl>
2024-10-21 17:17:37 +0200

    feat. vercel backend

---
+++ backend/api/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+
+const groceryRouter = require("./routes/groceries.js");
+const recipeRouter = require("./routes/recipes.js");
+
+const server = express();
+
+server.use(bodyParser.json());
+server.use(cors());
+
+server.use("/groceries", groceryRouter);
+server.use("/recipes", recipeRouter);
+// server.get("/*", function (req, res) {
+//     res.sendFile(path.join(__dirname, "dist/index.html"), function (err) {
+//     if (err) {
+//         res.status(500).send(err);
+//     }
+//     });
+// });
+server.get("/", function (req, res) {
+    const options = {
+        root: path.join(__dirname),
+    };
+
+    res.sendFile("index.html", options, function (err) {
+        if (err) {
+            console.error("Error sending file:", err);
+        } else {
+            console.log("Sent:", fileName);
+        }
+    });
+});
+
+server.listen(process.env.PORT || 8000, function (err) {
+    if (err) console.error(err);
+    console.log("Server listening...");
+});
+
+module.exports = server;
--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,43 @@
-const express = require("express");
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path')
+require("./api/index.js");
+// const express = require("express");
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const path = require('path')
 
-const groceryRouter = require('./routes/groceries.js');
-const recipeRouter = require('./routes/recipes.js');
+// const groceryRouter = require('./routes/groceries.js');
+// const recipeRouter = require('./routes/recipes.js');
 
-const server = express();
+// const server = express();
 
-server.use(bodyParser.json());
-server.use(cors());
+// server.use(bodyParser.json());
+// server.use(cors());
 
-server.use('/groceries', groceryRouter);
-server.use('/recipes', recipeRouter);
-// server.get("/*", function (req, res) {
-//     res.sendFile(path.join(__dirname, "dist/index.html"), function (err) {
-//     if (err) {
-//         res.status(500).send(err);
-//     }
+// server.use('/groceries', groceryRouter);
+// server.use('/recipes', recipeRouter);
+// // server.get("/*", function (req, res) {
+// //     res.sendFile(path.join(__dirname, "dist/index.html"), function (err) {
+// //     if (err) {
+// //         res.status(500).send(err);
+// //     }
+// //     });
+// // });
+// server.get('/', function (req, res) {
+//     const options = {
+//         root: path.join(__dirname)
+//     };
+
+//     res.sendFile('index.html', options, function (err) {
+//         if (err) {
+//             console.error('Error sending file:', err);
+//         } else {
+//             console.log('Sent:', fileName);
+//         }
 //     });
 // });
-server.get('/', function (req, res) {
-    const options = {
-        root: path.join(__dirname)
-    };
 
-    res.sendFile('index.html', options, function (err) {
-        if (err) {
-            console.error('Error sending file:', err);
-        } else {
-            console.log('Sent:', fileName);
-        }
-    });
-});
+// server.listen(process.env.PORT || 8000, function (err) {
+//     if (err) console.error(err);
+//     console.log("Server listening...")
+// });
 
-server.listen(process.env.PORT || 8000, function (err) {
-    if (err) console.error(err);
-    console.log("Server listening...")
-});
-
-module.exports = server
-
+// module.exports = server
